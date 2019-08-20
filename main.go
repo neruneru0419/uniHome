8b8c42cd444f8254df79acfbe3717e228212beb0
neruneru0419 <neruneru55555@gmail.com>
2019-08-20 23:42:50 +0900

    リクエストを投げると結果を返す

--- main.go
+++ main.go
@@ -1,21 +1,59 @@
+
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+  "net/http"
+  "io"
+  "encoding/json"
+  "fmt"
+  "strconv"
 )
+
+func dumpJsonRequestHandlerFunc(w http.ResponseWriter, req *http.Request){
+  //Validate request
+  if req.Method != "POST" {
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+
+  if req.Header.Get("Content-Type") != "application/json" {
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+
+  //To allocate slice for request body
+  length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+
+  //Read body data to parse json
+  body := make([]byte, length)
+  length, err = req.Body.Read(body)
+  if err != nil && err != io.EOF {
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+
+  //parse json
+  var jsonBody map[string]interface{}
+  err = json.Unmarshal(body[:length], &jsonBody)
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+  fmt.Printf("%v\n", jsonBody)
+
+  w.WriteHeader(http.StatusOK)  
+}
+
 func main(){
-	port := os.Getenv("PORT")
-	if port == ""{
+  port := os.Getenv("PORT")
+  if port == ""{
 		log.Fatal("$PORT must be set")
 	}
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", dumpJsonRequestHandlerFunc)
 	http.ListenAndServe(":"+port, nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
 }
