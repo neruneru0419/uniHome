06c5ed17516f43b9ca27a2ab57cfc770688afbd9
neruneru0419 <neruneru55555@gmail.com>
2019-08-21 09:23:20 +0900

    herokuへのデプロイテスト

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+	"io"
+	"encoding/json"
+	"fmt"
+	"strconv"
 	"os"
+	"log"
 )
 func main(){
 	port := os.Getenv("PORT")
@@ -16,6 +19,41 @@ func main(){
 	http.ListenAndServe(":"+port, nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", r.URL.Path[1:])
+func handler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	  }
+	
+	if req.Header.Get("Content-Type") != "application/json" {
+	w.WriteHeader(http.StatusBadRequest)
+	return
+	}
+
+	//To allocate slice for request body
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+	if err != nil {
+	w.WriteHeader(http.StatusInternalServerError)
+	return
+	}
+
+	//Read body data to parse json
+	body := make([]byte, length)
+	length, err = req.Body.Read(body)
+	if err != nil && err != io.EOF {
+	w.WriteHeader(http.StatusInternalServerError)
+	return
+	}
+
+	//parse json
+	var jsonBody map[string]interface{}
+	err = json.Unmarshal(body[:length], &jsonBody)
+	if err != nil {
+	w.WriteHeader(http.StatusInternalServerError)
+	return
+	}
+	fmt.Printf("%v\n", jsonBody)
+
+	w.WriteHeader(http.StatusOK)  
+	fmt.Fprintf(w, "Hello, %q", req.URL.Path[1:])
 }
