5e9f4006ffd0ae24755470a57748a09f801064e1
neruneru0419 <neruneru55555@gmail.com>
2019-08-23 22:45:58 +0900

    jsonでのデータの送受信

--- client.py
+++ client.py
@@ -1,18 +1,25 @@
 import time
+import json
+import random
 from websocket import create_connection
 from threading import Thread
 class Ws:
     def __init__(self):
         self.ws = create_connection("wss://neruneru.higashi.dev/sampleapp/ws")
+        self.unibo_id = random.randint(1, 100000)
+        print("userid="+str(self.unibo_id))
     def ws_send(self):
         while True:
             i = input()
-            self.ws.send(i)
+            data = {"user": self.unibo_id, "message": i}
+            jsn = json.dumps(data)
+            self.ws.send(jsn)
         ws.close()
     def ws_recv(self):
         while True:
-            result = self.ws.recv()
-            print("Received '%s'" % result) 
+            result = json.loads(self.ws.recv())
+            if result["user"] != self.unibo_id:
+                print("Received '%s'" % result["message"]) 
         ws.close()
 ws = Ws()
 wss = Thread(target=ws.ws_send)
