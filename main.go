1a7be02e3352cbf30eb38248d61c8110b67e48c1
neruneru0419 <neruneru55555@gmail.com>
2019-08-25 22:29:38 +0900

    微調整

--- main.go
+++ main.go
@@ -1,45 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "github.com/gin-gonic/autotls"
-    "github.com/gin-gonic/gin"
-    "gopkg.in/olahol/melody.v1"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
 )
 
 func main() {
-    log.Println("Websocket App start.")
+	log.Println("Websocket App start.")
 
-    router := gin.Default()
-    m := melody.New()
+	router := gin.Default()
+	m := melody.New()
 
-    rg := router.Group("/sampleapp")
-    rg.GET("/", func(ctx *gin.Context) {
-        http.ServeFile(ctx.Writer, ctx.Request, "index.html")
-    })
+	rg := router.Group("/sampleapp")
+	rg.GET("/", func(ctx *gin.Context) {
+		http.ServeFile(ctx.Writer, ctx.Request, "index.html")
+	})
 
-    rg.GET("/ws", func(ctx *gin.Context) {
-        m.HandleRequest(ctx.Writer, ctx.Request)
-    })
+	rg.GET("/ws", func(ctx *gin.Context) {
+		m.HandleRequest(ctx.Writer, ctx.Request)
+	})
 
-    m.HandleMessage(func(s *melody.Session, msg []byte) {
-        m.Broadcast(msg)
-        fmt.Println(msg)
-    })
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+		fmt.Println(msg)
+	})
 
-    m.HandleConnect(func(s *melody.Session) {
-        log.Printf("websocket connection open. [session: %#v]\n", s)
-    })
+	m.HandleConnect(func(s *melody.Session) {
+		log.Printf("websocket connection open. [session: %#v]\n", s)
+	})
 
-    m.HandleDisconnect(func(s *melody.Session) {
-        log.Printf("websocket connection close. [session: %#v]\n", s)
-    })
+	m.HandleDisconnect(func(s *melody.Session) {
+		log.Printf("websocket connection close. [session: %#v]\n", s)
+	})
 
-        // Listen and server on 0.0.0.0:8989
-    //router.Run(":443")
-    log.Fatal(autotls.Run(router, "neruneru.higashi.dev"))
+	// Listen and server on 0.0.0.0:8989
+	//router.Run(":443")
+	log.Fatal(autotls.Run(router, "neruneru.higashi.dev"))
 
-    fmt.Println("Websocket App End.")
+	fmt.Println("Websocket App End.")
 }
