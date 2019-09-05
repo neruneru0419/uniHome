a2160047a8acd818731a95d7c025516da170afb0
biseki <asaiict1313@gmail.com>
2019-09-05 13:04:24 +0900

    [clean]

--- dance.py
+++ dance.py
@@ -3,7 +3,6 @@ import time
 
 def dance():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(14, GPIO.IN)
 
     GPIO.setup(24, GPIO.OUT)
     right = GPIO.PWM(24, 50)
@@ -22,20 +21,5 @@ def dance():
         right.ChangeDutyCycle(9.625)
         left.ChangeDutyCycle(12)
         time.sleep(0.5)
-"""
-print("start")
 
-
-j = 0
-while j < 1.5:
-    if GPIO.input(14) != 0:
-        j += 0.1
-    else:
-        j = 0
-
-    time.sleep(0.1)
-dance()
-
-print("finish")
-"""
-GPIO.cleanup()
+    GPIO.cleanup()
