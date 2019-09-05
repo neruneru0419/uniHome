1bc49aa04feabc3db7d996087e2719ae940c8928
biseki <asaiict1313@gmail.com>
2019-09-05 13:03:28 +0900

    [clean]

--- audio.py
+++ audio.py
@@ -4,16 +4,11 @@ import subprocess
 
 def sound():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(14, GPIO.IN)
 
     CMD = "aplay /home/pi/hello-Pi/トーマス.wav"
     try:
-        while True:
-            if GPIO.input(14):
-                subprocess.call(CMD, shell=True)
-            time.sleep(0.01)
+        subprocess.call(CMD, shell=True)
 
     except KeyboardInterrupt:
         pass
-
-GPIO.cleanup()
+#sound()
