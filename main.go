1c592bec5c2e97eae628342d6da3729b552ac355
neruneru0419 <neruneru55555@gmail.com>
2019-09-22 00:38:30 +0900

    log機能完成

--- main.go
+++ main.go
@@ -1,31 +1,15 @@
 package main
 
 import (
-	"./uniboLog"
+	"encoding/json"
 	"fmt"
 	"log"
-	"os"
-	"io/ioutil"
-<<<<<<< HEAD
-	"encoding/json"
-=======
 
->>>>>>> 02017cf161a307e2d50d6298daf0ef11c88dd0be
+	"./uniboLog"
 	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
 )
-func fileRead()(string){
-	file, err := os.Open("./UniboLog.txt")
-	if err != nil{
-		fmt.Println("FileReadError")
-	}
-	defer file.Close()
-
-	log, err := ioutil.ReadAll(file)
-
-	return string(log)
-}
 
 func main() {
 	log.Println("Websocket App start.")
@@ -36,22 +20,6 @@ func main() {
 
 	rg.GET("/ws", func(ctx *gin.Context) {
 		m.HandleRequest(ctx.Writer, ctx.Request)
-	})
-	rg.GET("/api/log", func(ctx *gin.Context) {
-		uniboLog = fileRead()
-		ctx.JSON(200, gin.H{
-			"message": uniboLog,
-		})
-	})
-	rg.GET("/api/reaction", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "hello, world",
-		})
-	})
-	rg.POST("/api/action", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "hello, world",
-		})
 	})
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
